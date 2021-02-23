314e948d0e54afd897687bf88e9dce6ba75d62bb
zty <zty@ztydeMBP.lan>
2021-02-23 20:42:44 +0800

    替换文件

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,12 +1,11 @@
-#include <jni.h>
+#include "jni.h"
 #include <string>
 extern "C"{
-#include <>
+#include <libavutil/avutil.h>
 }
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_withc_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+    return env->NewStringUTF("av_version_info()");
 }
