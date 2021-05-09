4a97f1478efa240c009d55d8562e174ec606db7d
ZhangTianyi <965819494@qq.com>
2021-05-09 11:55:36 +0800

    替换静态库

--- build.sh
+++ build.sh
@@ -1,46 +1,79 @@
 #!/bin/bash
-#NDK 配置
-NDK_ROOT=/Users/zty/Library/Android/sdk/ndk/21.3.6528147
-#TOOLCHAIN
-TOOLCHAIN=$NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64
-#支持的CPU
-CPU=arm-linux-androideabi
-#NDK环境设置
-ADDI_LDFLAGS="-fPIE -pie"
-ADDI_CFLAGS="-fPIE -pie -march=armv7-a -mfloat-abi=softfp -mfpu=neon"
+# 搭配ffmpeg 4.4/ 4.2.2/ 3.4.6测试正常
+# 修改为你本地NDK目录
+export NDK=/Users/zty/Library/Android/sdk/ndk/22.1.7171670
+# 当前系统
+export HOST_PLATFORM=darwin-x86_64
+export TOOLCHAIN=$NDK/toolchains/llvm/prebuilt/$HOST_PLATFORM
+export SYSROOT=$NDK/toolchains/llvm/prebuilt/$HOST_PLATFORM/sysroot
+export API=21
 
+function build_android() {
+    echo "Compiling FFmpeg for $CPU"
+    ./configure \
+	--prefix=$PREFIX \
+	--ar=$TOOLCHAIN/bin/${TARGET}-ar \
+	--as=$TOOLCHAIN/bin/${TARGET}-as \
+	--cc=$TOOLCHAIN/bin/${TARGET_MIN}-clang \
+	--cxx=$TOOLCHAIN/bin/${TARGET_MIN}-clang++ \
+	--nm=$TOOLCHAIN/bin/${TARGET}-nm \
+	--ranlib=$TOOLCHAIN/bin/${TARGET}-ranlib \
+	--strip=$TOOLCHAIN/bin/${TARGET}-strip \
+	--arch=$ARCH \
+	--target-os=android \
+	--enable-cross-compile \
+	--disable-asm \
+	--disable-shared \
+	--enable-static \
+	--disable-programs \
+	--disable-debug \
+	--disable-symver \
+	--enable-jni \
+	--enable-mediacodec \
+	--enable-decoder=h264_mediacodec \
+	--enable-hwaccel=h264_mediacodec \
+    --enable-decoder=hevc_mediacodec \
+    --enable-decoder=mpeg4_mediacodec \
+    --enable-decoder=vp8_mediacodec \
+    --enable-decoder=vp9_mediacodec \
+	--enable-small \
+	--disable-debug \
+	--enable-neon \
+	--enable-gpl \
+	--enable-version3 \
+	--enable-nonfree \
+	--disable-avdevice \
+	--extra-cflags="-Os -fpic $OPTIMIZE_CFLAGS" \
+	--extra-ldflags="$ADDI_LDFLAGS"
 
-#配置文件的生成目录
-./configure \
---prefix=`pwd`/android/armeabi-v7 \
-#生成文件尽可能小
---enable-small \
-#不直接使用ffmpeg可执行文件 而是通过程序调用
---disable-programs \
-#无用的摄像头驱动（Android的不行）
---disable-avdevice \
-#后期处理模块
---disable-postproc \
-#关闭编码
---disable-encoders \
-#关闭封装音视频
---disable-muxers \
-#允许交叉编译
---enable-cross-compile \
-#使用对应CPU
---cross-prefix=$TOOLCHAIN/bin/$CPU- \
-#指定使用这个目录下usr 里面的头文件和库
---sysroot=$NDK_ROOT/platforms/android-21/arch-arm \
+    make clean
+    make -j8
+    make install
+    echo "The Compilation of FFmpeg for $CPU is completed"
+}
 
---extra-cflags="$ADDI_CFLAGS" \
+## armv8-a
+function armv8() {
+    ARCH="arm64"
+    CPU="armv8-a"
+	TARGET="aarch64-linux-android"
+    TARGET_MIN="aarch64-linux-android${API}"
+    PREFIX="${PWD}/android/arm64-v8a"
+    OPTIMIZE_CFLAGS="-march=$CPU"
+    build_android
+}
 
---extra-ldflags="$ADDI_LDFLAGS" \
+## armv7-a
+function armv7() {
+    ARCH="arm"
+    CPU="armv7-a"
+	TARGET="arm-linux-androideabi"
+    TARGET_MIN="armv7a-linux-androideabi${API}"
+    PREFIX="${PWD}/android/armeabi-v7a"
+    OPTIMIZE_CFLAGS="-march=$CPU"
+    build_android
+}
 
---arch=arm \
---target-os=android 
-
-make clean
-make -j4 install
-
-
-
+# 根据自己需要的 cpu, 放开对应的注释即可
+armv8
+armv7
